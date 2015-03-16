169dec0fc04a5a323c9d4f3f0078778aaf28da37
Neel Smith <dnsmith.neel@gmail.com>
2015-03-16 05:47:09 -0400

    Move texts within specs area

---
+++ specs/java/ptolemy/dependencies/DependenciesTest.java
@@ -0,0 +1,10 @@
+package ptolemy.dependencies;
+
+
+import org.concordion.integration.junit3.ConcordionTestCase;
+
+public class DependenciesTest extends ConcordionTestCase {
+
+}
+
+
---
+++ specs/md/ptolemy/dependencies/Dependencies.md
@@ -0,0 +1,9 @@
+# Dependencies #
+
+
+This version of `ptolemy` was compiled against:
+
+<table>
+<tr><th>Package</th><th>Version</th></tr>
+@deps@	
+</table>
