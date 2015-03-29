fc94c507a6c65bbfc8da05e232b72151e53aec63
Neel Smith <dnsmith.neel@gmail.com>
2015-03-28 20:58:45 -0400

    Add placeholder class

---
+++ src/main/groovy/edu/holycross/shot/ptolemy/SpatialAnalyst.groovy
@@ -0,0 +1,23 @@
+package edu.holycross.shot.ptolemy
+
+
+/**
+ * A class analyzing spatial density and implied precision of
+ * Ptolemy's data by province, as well as checking correlations of 
+ * all properties of Ptolemaic points.
+ * It would be cool to do this with geotools, but I can't get them
+ * working. Perhaps work with data generated from R.
+ */
+class SpatialAnalyst {
+
+  /*
+- for each point in a province: find its twelfth value.
+- find most precise fraction in province
+- test distribution of twelfths against expected distributtion for implied
+precision.
+   */
+
+  
+  SpatialAnalyst(){
+  }
+}
